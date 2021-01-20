91c601dc900e18db38338bd071edb418b3304141
Jesse Willard <jesse.willard@gmail.com>
2021-01-20 01:04:33 -0800

    Excellent Submission

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [init, setInit] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if (!init) {
+      const endpoints = [];
+      endpoints.push(
+        "https://my-json-server.typicode.com/typicode/demo/posts/1"
+      );
+      endpoints.push(
+        "https://my-json-server.typicode.com/typicode/demo/posts/2"
+      );
+      endpoints.push(
+        "https://my-json-server.typicode.com/typicode/demo/posts/3"
+      );
+      const limit = endpoints.length;
+      let range = {
+        from: 0,
+        to: limit,
+        [Symbol.asyncIterator]() {
+          return {
+            current: this.from,
+            last: this.to,
+
+            async next() {
+              const value = await new Promise((resolve) => {
+                const endpoint = endpoints[this.current];
+                if (endpoint) {
+                  setTimeout(async () => {
+                    fetch(endpoint)
+                      .then((response) => response.json())
+                      .then((data) => {
+                        resolve(`${endpoints[this.current]}`);
+                      })
+                      .finally(() => {})
+                  }, 1000)
+                } else {
+                  resolve();
+                }
+              });
+              if (this.current < this.last ) {
+                this.current++;
+                return { done: false, value };
+              } else {
+                return { done: true };
+              }
+            }
+          };
+        }
+      };
+
+      (async () => {
+        const collection = [];
+        for await (let value of range) {
+          collection.push(value);
+          setPosts([...collection]);
+        }
+      })();
+      setInit(true);
+    }
+  }, [init]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts?.map((value, index) => (
+        <div key={`${index}`}>{value}</div>
+      ))}
+      {!posts && <></>}
     </div>
   );
 }
-
-export default App;
